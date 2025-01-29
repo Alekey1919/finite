849a6e2314c1b4dfb06471b3100f5bc17a724a23
Alejo Espindola <alejosa1919@gmail.com>
2025-01-28 22:39:05 -0300

    Remove themes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import useColorTheme from "../hooks/useColorTheme";
-
 const Navbar = () => {
-  const { switchColorTheme } = useColorTheme();
-
   return (
     <nav className="flex justify-end">
-      <span onClick={switchColorTheme}>Theme</span>
+      {/* <span onClick={switchColorTheme}>Theme</span> */}
     </nav>
   );
 };
